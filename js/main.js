fdbecff4681e90c3b944843c97dc0850b937a55c
johnmarkbondad <johnmarkbondad333@gmail.com>
2023-03-14 17:57:32 +0800

    fix slideshow in Services section in Home page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
   });
 });
 
+// spinner/loader
+
+const spinnerWrapperEl = document.querySelector(".spinner-wrapper");
+
+window.addEventListener("load", () => {
+  spinnerWrapperEl.style.opacity = "0";
+  setTimeout(() => {
+    spinnerWrapperEl.style.display = 'none';
+  }, 200)
+});
+
+
 
     // Carousel for Genius Team
     let items = document.querySelectorAll('.carousel .genius-team-carousel')
